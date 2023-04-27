b87bb0fc469d22e06c043f4139bf4e4bcf481a1f
pawel <pawel.boryszewski@codino.pl>
2023-04-27 11:14:23 +0200

    Added 2 methods to dao to get last entity, defined logic for saving events in JournalRepository

--- app/src/main/java/com/pbl9/pomodoro/db/JournalDao.kt
+++ app/src/main/java/com/pbl9/pomodoro/db/JournalDao.kt
@@ -2,11 +2,19 @@ package com.pbl9.pomodoro.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.pbl9.pomodoro.models.db.JournalEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface JournalDao {
 
+    @Query("SELECT * FROM journal LIMIT 1")
+    suspend fun getLastEntity(): JournalEntity?
+
+    @Query("SELECT * FROM journal LIMIT 1")
+    fun getLastEntityFlow(): Flow<JournalEntity>
+
     @Insert
-    fun addEntity(entity: JournalEntity)
+    suspend fun addEntity(entity: JournalEntity)
 }
--- app/src/main/java/com/pbl9/pomodoro/repository/JournalRepository.kt
+++ app/src/main/java/com/pbl9/pomodoro/repository/JournalRepository.kt
@@ -1,7 +1,97 @@
 package com.pbl9.pomodoro.repository
 
 import com.pbl9.pomodoro.db.JournalDao
+import com.pbl9.pomodoro.models.db.JournalEntity
+import com.pbl9.pomodoro.models.db.JournalEvent
 
 class JournalRepository(private val journalDao: JournalDao) {
 
+    suspend fun saveEvent(event: JournalEvent) {
+        val lastEntity = journalDao.getLastEntity()
+        val entity = if(lastEntity == null) { //database is empty, start new session
+            if(event == JournalEvent.SESSION_START) JournalEntity(event = event, sessionNumber = 1, elapsedTime = 0)
+            else throw IllegalStateException("Illegall state, database is empty so first event must be SESSION_START")
+        } else {
+            createNewEntityBasedOnPrevious(event, lastEntity)
+        }
+        journalDao.addEntity(entity)
+    }
+
+    suspend fun resume() {
+        val lastEntity = journalDao.getLastEntity() ?: return
+        lastEntity.previousEvent?.let {
+            when (it) {
+                JournalEvent.SESSION_RESUME, JournalEvent.SESSION_START -> {
+                    JournalEntity(event = JournalEvent.SESSION_RESUME, sessionNumber = lastEntity.sessionNumber, elapsedTime = lastEntity.elapsedTime, previousEvent = JournalEvent.PAUSE)
+                }
+                JournalEvent.BREAK_START, JournalEvent.BREAK_RESUME -> {
+                    JournalEntity(event = JournalEvent.BREAK_RESUME, sessionNumber = lastEntity.sessionNumber, elapsedTime = lastEntity.elapsedTime, previousEvent = JournalEvent.PAUSE)
+                }
+                else -> {
+                    null
+                }
+            }
+        }?.let {
+            journalDao.addEntity(it)
+        }
+    }
+
+    fun getLastJournalFlow() = journalDao.getLastEntityFlow()
+
+    private fun createNewEntityBasedOnPrevious(
+        eventToSave: JournalEvent,
+        previous: JournalEntity
+    ) =
+        when (eventToSave) {
+            JournalEvent.SESSION_START -> {
+                JournalEntity(
+                    event = eventToSave,
+                    sessionNumber = previous.sessionNumber + 1,
+                    elapsedTime = 0,
+                    previousEvent = previous.event
+                )
+            }
+            JournalEvent.BREAK_START -> {
+                JournalEntity(
+                    event = eventToSave,
+                    sessionNumber = previous.sessionNumber,
+                    elapsedTime = 0,
+                    previousEvent = previous.event
+                )
+            }
+            JournalEvent.PAUSE -> {
+                val now = System.currentTimeMillis()
+                val newElapsedTime = previous.elapsedTime + now - previous.timestamp
+                JournalEntity(
+                    event = eventToSave,
+                    sessionNumber = previous.sessionNumber,
+                    elapsedTime = newElapsedTime,
+                    previousEvent = previous.event
+                )
+            }
+            JournalEvent.SESSION_RESUME -> {
+                JournalEntity(
+                    event = eventToSave,
+                    sessionNumber = previous.sessionNumber,
+                    elapsedTime = previous.elapsedTime,
+                    previousEvent = previous.event
+                )
+            }
+            JournalEvent.BREAK_RESUME -> {
+                JournalEntity(
+                    event = eventToSave,
+                    sessionNumber = previous.sessionNumber,
+                    elapsedTime = previous.elapsedTime,
+                    previousEvent = previous.event
+                )
+            }
+        }
+
+
 }
+/*
+* 1. Start -> timestamp = 0, elapsedTime = 0
+* 2. Pause -> timestamp = x, elapsedTime = x
+* 3. Start -> timestamp = y, elapsedTime = x
+* 4. Pause -> timestamp = y + z, elapsedTime = x + z - y
+* */
