f0ec8879135a380febea82f9c6676c1a0277deb9
pawel <pawel.boryszewski@codino.pl>
2023-04-27 11:13:02 +0200

    Added new field previousEvent to JournalEntity, added default value for id

--- app/src/main/java/com/pbl9/pomodoro/models/db/JournalEntity.kt
+++ app/src/main/java/com/pbl9/pomodoro/models/db/JournalEntity.kt
@@ -4,11 +4,12 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "journal")
 data class JournalEntity(
-    @PrimaryKey(autoGenerate = true) val id: Long,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
+    @ColumnInfo(name = "timestamp") val timestamp: Long = System.currentTimeMillis(),
     @ColumnInfo(name = "session_number") val sessionNumber: Int,
-    @ColumnInfo(name = "timestamp") val timestamp: Long,
     @ColumnInfo(name = "event") val event: JournalEvent,
     @ColumnInfo(name = "elapsed_time") val elapsedTime: Long,
+    @ColumnInfo(name = "prevoius_event") val previousEvent: JournalEvent? = null
 )
